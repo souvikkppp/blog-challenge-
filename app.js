76fc924ddf9e867690aa068099753094387796ca
soovik kundu <sovikkunducom19@gmail.com>
2022-08-22 08:38:24 +0530

    challenge 12 to 18

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,22 +13,44 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 const posts = [];
 
 app.get("/", (req, res) => {
-  res.render("home", {homeStartingContent: homeStartingContent});
-  console.log(posts);
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts
+  });
+
+
 });
+app.get('/posts/:story', (req, res) => {
+let reqParams = _.upperCase(req.params.story);
+  posts.forEach((post) => {
+    let postTitle = _.upperCase(post.title);
+
+    if (postTitle === reqParams) {
+      console.log("match found");
+    } else {
+      console.log("not found");
+    }
+  });
+})
 
 app.get("/about", (req, res) => {
-  res.render("about", {aboutContent: aboutContent});
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact", {
+    contactContent: contactContent
+  });
 });
 
 app.get("/compose", (req, res) => {
@@ -39,11 +62,10 @@ app.post("/compose", (req, res) => {
     title: req.body.titleBody,
     post: req.body.postBody
   };
-   posts.push(post);
-   res.redirect("/")
+  posts.push(post);
+
+  res.redirect("/")
 });
-
-
 
 
 
